0dbc14ca1a5cc08dc93b7c453dd7ac0054ba716b
Art <absolutus11@gmail.com>
2024-10-08 03:49:17 +0400

    feat: cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,22 @@
+import { fork } from "child_process";
+import path from "node:path";
+import {fileURLToPath} from "node:url";
+
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const filename = path.join(__dirname, "files", "script.js");
+    const cp = fork(filename, args);
+
+    cp.on("message", (chunk) => {
+        process.stdout.write(`Received from child process: ${chunk.toString()}\n`)
+    });
+
+    cp.on("close", (chunk) => {
+        process.stdout.write(`Process closed: ${chunk.toString()}\n`)
+    });
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(process.argv);
